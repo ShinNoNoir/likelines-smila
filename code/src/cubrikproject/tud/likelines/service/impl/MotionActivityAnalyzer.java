68a659cb2b5f7159ff197e59d88d5b2de0c2cc52
Raynor Vliegendhart <shinnonoir+git@gmail.com>
2013-10-14 12:29:22 +0200

    Add post-processing to Motion Analysis

--- code/src/cubrikproject/tud/likelines/service/impl/MotionActivityAnalyzer.java
+++ code/src/cubrikproject/tud/likelines/service/impl/MotionActivityAnalyzer.java
@@ -15,9 +15,15 @@ public class MotionActivityAnalyzer {
 	/** path to the binary */
 	private final String motionActivityPath;
 	
+	/** Special output code by the binary for indicating a skipped frame */
 	private final int SKIPPED_FRAME = -1;
+	/** Special output code by the binary for indicating a frame without a motion vector field */
 	private final int NO_MOTION = -2;
+	/** Special output code by the binary for indicating a duplicate frame */
 	private final int DUPLICATE_FRAME = -3;
+	
+	/** Clipping threshold */
+	private final int CLIPPING_THRESHOLD = 32;
 	
 	/**
 	 * Constructs a MotionActivityAnalyzer object.
@@ -41,24 +47,94 @@ public class MotionActivityAnalyzer {
 		BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 		String line;
 		
-		ArrayList<Double> values = new ArrayList<Double>();
-		double[] scores;
-		
 		// read proc's STDOUT
+		ArrayList<Double> values = new ArrayList<Double>();
 		while ((line = br.readLine()) != null) {
 			values.add(Double.parseDouble(line));
 		}
 				
-		scores = new double[values.size()];
-		for (int i = 0; i < scores.length; i++) {
-			double score = values.get(i);
-			scores[i] = (score == NO_MOTION) ? 0 : score;
-		}
-		
-		// TODO: process SKIPPED_FRAME and DUPLICATE_FRAME
+		double[] scores = processSpecialFrames(values);
+		applyClippingAndNormalize(scores);
+		applyMedianFilter(scores);
 		
 		return scores;
 	}
+
+	/**
+	 * Applies a median filter using a sliding window of 3 elements
+	 * @param scores The results of the analysis
+	 */
+	private void applyMedianFilter(double[] scores) {
+		double[] scores_pre = scores.clone();
+		for (int k = 1; k < scores.length - 1; k++)
+			scores[k] = median(scores_pre[k-1], scores_pre[k], scores_pre[k+1]);
+	}
+
+	/**
+	 * Apply clipping to the extracted motion and normalize.
+	 * 
+	 * @param scores The array to apply the clipping process to
+	 */
+	private void applyClippingAndNormalize(double[] scores) {
+		for (int k = 0; k < scores.length; k++) {
+			if (scores[k] > CLIPPING_THRESHOLD)
+				scores[k] = CLIPPING_THRESHOLD;
+			
+			scores[k] /= CLIPPING_THRESHOLD;
+		}
+	}
+
+	/**
+	 * Post-processes the binary output to deal with special frames.
+	 * 
+	 * @param values Output produced by the binary
+	 * @return An array with special frames (negative values) replaced with correct values
+	 */
+	private double[] processSpecialFrames(ArrayList<Double> values) {
+		double[] scores = new double[values.size()];
+		for (int k = 0; k < scores.length; k++) {
+			double score = scores[k] = values.get(k);
+			
+			if (score == NO_MOTION) {
+				scores[k] = 0;
+			}
+			else if (score == SKIPPED_FRAME || score == DUPLICATE_FRAME) {
+				// Find previous and next non-negative entries
+				int pre = -1;
+				int post = -1;
+				
+				for (int i = k-1; 0 <= i; i--)
+					if (scores[i] >= 0) {
+						pre = i;
+						break;
+					}
+				for (int i = k+1; i < scores.length; i++)
+					if (scores[i] >= 0) {
+						post = i;
+						break;
+					}
+				
+				if (pre == -1 && post == -1)
+					scores[k] = 0;
+				else if (pre == -1)
+					scores[k] = scores[post];
+				else if (post == -1)
+					scores[k] = scores[pre];
+				else
+					scores[k] = 0.5 * (scores[pre] + scores[post]);
+			}
+		}
+		return scores;
+	}
 	
-	
+	/**
+	 * Computes the median of a triplet
+	 * @param a First value
+	 * @param b Second value
+	 * @param c Third value
+	 * @return The median of the given triplet
+	 */
+	private double median(double a, double b, double c) {
+		return Math.max(Math.min(a,b), Math.min(Math.max(a,b),c));
+	}
 }
